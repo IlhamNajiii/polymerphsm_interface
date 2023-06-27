78600ad17f5750ef7ecf320e3e8a6aebca55e101
IlhamNajiii <127828076+IlhamNajiii@users.noreply.github.com>
2023-06-27 15:40:19 +0700

    Add files via upload

---
+++ Canon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismInterface
+{
+    public class Canon : IPrinter
+    {
+        public void Speak(PrinterWindows printerWindows)
+        {
+            Console.WriteLine("Canon printer printing....");
+            Console.WriteLine("Canon display dimension : 10*11");
+        }
+    }
+}
+
---
+++ Epson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolymorphismInterface;
+
+namespace PolymorphismInterface
+{
+    public class Epson : IPrinter
+    {
+        public void Speak(PrinterWindows printerWindows)
+        {
+            Console.WriteLine("Epson printer printing....");
+            Console.WriteLine("Printer Windows display dimension : 10*11");
+        }
+    }
+}
